a5ae8814b5d4971feb6db8a6c74bde8efabbd197
Naufal Dwar <Naufaldwar>
2022-04-20 07:15:49 +0700

    Tugas Pertemuan 7

---
+++ shared_preferences/lib/data_dummy.dart
@@ -0,0 +1,46 @@
+class AppStore {
+  String name;
+  String imageLogo;
+  String jenis;
+
+  AppStore({required this.name, required this.imageLogo, required this.jenis});
+}
+
+var appList = [
+  //1
+  AppStore(
+    jenis: "Baju Lebaran Anak",
+    name: "Sakeena Gamis",
+    imageLogo:
+        "https://id-live-01.slatic.net/p/f54f056d29590a6cdf7294d37c62ec24.jpg",
+  ),
+
+  //2
+  AppStore(
+    jenis: "Baju Lebaran Anak",
+    name: "Syafiyah Series",
+    imageLogo: "https://s1.bukalapak.com/img/11045960272/large/data.jpeg",
+  ),
+
+  //3
+  AppStore(
+    jenis: "Baju Lebaran Anak",
+    name: "Thalita Series",
+    imageLogo: "https://cf.shopee.co.id/file/81969e58e57e9e6e1824421b792a8e81",
+  ),
+
+  //4
+  AppStore(
+    jenis: "Baju Koko Anak",
+    name: "Koko Kids Jefri",
+    imageLogo: "https://cf.shopee.co.id/file/94350dde43bc9b9ed7ba0dbedcda93de",
+  ),
+
+  //5
+  AppStore(
+    jenis: "Baju Koko Anak",
+    name: "Little Muslim",
+    imageLogo:
+        "https://id-test-11.slatic.net/p/43a3308b5550ad9dc65d5dcaf6d7f07a.jpg",
+  ),
+];
